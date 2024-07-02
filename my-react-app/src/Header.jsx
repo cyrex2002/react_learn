8995571b5d39046318ee53e1f06fd1a23b59a683
Senuja Jayasekara <senujajayaekara@gmail.com>
2024-07-02 23:09:08 +0530

    first commit

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header(params) {
+    return(
+        <header>
+            <h1>Senuja Jayasekara</h1>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <hr />
+            </ul>
+        </header>
+    )
+}
+
+export default Header
