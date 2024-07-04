5bc578cf7142d59873fd103b1b04eaa8bb30a508
senuja <senujajayasekara@gmail.com>
2024-07-04 19:14:48 +0530

    Add Food and Card components

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img src="https://via.placeholder.com/150" alt="Food Picture" />
+            <h2>Apple</h2>
+            <p>Apple is a fruit</p>
+        </div>   
+    );
+}
+
+export default Card;
---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,11 @@
+function Food(){
+    const food1 = 'apple';
+    const food2 = 'banana';
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+export default Food
