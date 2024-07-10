29c05d14e39de30165a0439fedd7238e711b514e
senuja <senujajayasekara@gmail.com>
2024-07-10 19:07:10 +0530

    2024-07-10

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,14 +2,17 @@ import Header from './Header.jsx'
 import Footer from "./Footer.jsx";
 import Food from './Food.jsx';
 import Card from './Card.jsx';
+
+import food1 from './assets/apple_img.jpg';
+import food2 from './assets/orange_img.jpg';
+
 function App() {
   return (
     <>
     <Header/>
     <Food/>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Card name="Apple" image={food1}/>
+    <Card name="Orange" image={food2}/>
     <Footer/> 
     </>
   );
--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,10 +1,11 @@
-import food1 from './assets/apple.jpg';
-function Card() {
+
+function Card(props) {
+
     return (
         <div className="card">
-            <h2 className='card-title'>Apple</h2>
-            <img className="card-image" src={food1} alt="Food Picture" />
-            <p className='card-text'>Apple is a fruit</p>
+            <h2 className='card-title'>{props.name}</h2>
+            <img className="card-image" src={props.image} alt="Food Picture" />
+            <p className='card-text'>{props.name} is a fruit.</p>
         </div>   
     );
 }
