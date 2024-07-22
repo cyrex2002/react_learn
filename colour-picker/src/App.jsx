8ad74e3a21006e2826d9cec4829599cbdcd13c26
senuja <senujajayasekara@gmail.com>
2024-07-22 13:46:10 +0530

    colour picker app

---
+++ colour-picker/src/App.jsx
@@ -0,0 +1,13 @@
+import ColourPicker from "./ColourPicker"
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <ColourPicker/>
+    </>
+  )
+}
+
+export default App
