e7a55c26298fb0543977e4d0810ceb4e0b47b85c
senuja <senujajayasekara@gmail.com>
2024-07-28 20:57:58 +0530

    finished

--- to-do-list/src/ToDoComponent.jsx
+++ to-do-list/src/ToDoComponent.jsx
@@ -2,25 +2,81 @@ import { useState } from "react";
 
 function TodoList() {
   const [taskList, setTaskList] = useState([]);
+  const [taskInput, setTaskInput] = useState("");
 
   function handleTaskInput(event) {
-    const newTask = event.target.value;
-    setTaskList(previousTaskList => [...previousTaskList, newTask]);
-    event.target.value = "";
+    setTaskInput(event.target.value);
+  }
+
+  function addTask() {
+    if(taskInput.trim() !== ""){
+    setTaskList(t => [...t, taskInput]);
+    setTaskInput("");
+    }
+  }
+
+  function deleteTask(index){
+    const updatedTaskList = taskList.filter((task, i) => i !== index);
+    setTaskList(updatedTaskList);
+
+  }
+  function moveUp(index){
+    
+    if(index>0){
+      const updatedTaskList = [...taskList]; 
+      [updatedTaskList[index-1], updatedTaskList[index]] =
+      [updatedTaskList[index], updatedTaskList[index-1]]; //swapping
+      setTaskList(updatedTaskList); 
+    }
+    
+
+  }
+
+  function moveDown(index){
+    if(index<taskList.length-1){
+      const updatedTaskList = [...taskList]; 
+      [updatedTaskList[index], updatedTaskList[index+1]] =
+      [updatedTaskList[index+1], updatedTaskList[index]]; //swapping
+      setTaskList(updatedTaskList); 
+    }
+
   }
 
   return (
     <>
+    <div className="container">
       <h1>To-Do-List</h1>
-      <div>
-        <input type="text" onChange={handleTaskInput} />
-        <button>Add task</button>
+      <div className="input-conatiner">
+        <input 
+            className="input"
+            type="text" 
+            placeholder="Add a task"
+            value={taskInput}
+            onChange={handleTaskInput} 
+        />
+        <button className="add-button"
+                 onClick={addTask}>Add task</button>
       </div>
-      <ol>
+      <ol className="list">
         {taskList.map((task, index) => (
-          <li key={index}>{task}</li>
+          <li key={index}>
+            <span className="list-text">{task}</span>
+            <button 
+              className="move-button"
+              onClick={() => moveUp(index)}            
+            >👆</button>
+            <button 
+              className="move-button"
+              onClick={() => moveDown(index)}            
+            >👇</button>
+            <button 
+              className="delete-button"
+              onClick={() => deleteTask(index)}            
+            >delete</button>
+          </li>
         ))}
       </ol>
+      </div>  
     </>
   );
 }
